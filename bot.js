b6bcd9a8cc1348248804988bf2a3f152b3d4962e
WojanWariat <66416789+WojanWariat@users.noreply.github.com>
2020-06-04 12:30:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot
+client.login(process.env.NzE4MDQ4NzQ5OTc2NDIwMzc2.XtjNCA.eXOS2hI2fq-OVH4_hyV9Itw5WIw);//where BOT_TOKEN is the token of our bot
